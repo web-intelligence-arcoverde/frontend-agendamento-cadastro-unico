075f622d2213fe102226ba805192eb9d9ee82196
lucaspaes <lukas.paes18@gmail.com>
2021-11-25 10:51:55 -0300

    push origin

--- src/components/molecules/MenuConfig/index.tsx
+++ src/components/molecules/MenuConfig/index.tsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 import Clerks from 'src/components/atomic/Clerks'
 import { Container, Menu, SClick, Title } from './styled'
 
 const MenuConfig = () => {
-  const [visible, setVisible] = useState(false)
+  const [optionComponent, setOptionComponent] = useState('default')
+
+  const Components = {
+    data: <ComponentData />,
+    dia: <div />,
+    h: <div />,
+  }
 
   return (
     <Container>
@@ -12,18 +18,22 @@ const MenuConfig = () => {
         <h4>Atendentes | Datas | Horas</h4>
       </Title>
       <Menu>
-        <button onClick={() => setVisible(true)}>A</button>
-        {visible ? (
-          <SClick>
-            <Clerks />
-          </SClick>
-        ) : null}
-        <button onClick={() => setVisible(true)}>DT</button>
-        <button onClick={() => setVisible(true)}>D</button>
-        <button onClick={() => setVisible(true)}>H</button>
-        <button onClick={() => setVisible(true)}>HE</button>
+        <button onClick={() => setOptionComponent('a')}>A</button>
+        <button onClick={() => setOptionComponent('data')}>DT</button>
+        <button onClick={() => setOptionComponent('dia')}>D</button>
+        <button onClick={() => setOptionComponent('h')}>H</button>
+        <button onClick={() => setOptionComponent('default')}>HE</button>
       </Menu>
+      {Components[optionComponent]}
     </Container>
+  )
+}
+
+const ComponentData = () => {
+  return (
+    <SClick>
+      <Clerks />
+    </SClick>
   )
 }
 
